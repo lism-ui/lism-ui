646553e9bd6440a75efb9922c43d66a87f0ccf9b
ddryo <dd.bocsfcryo@gmail.com>
2024-07-01 10:42:37 +0900

    banner復活

--- packages/lism-core/src/components/Banner/getProps.js
+++ packages/lism-core/src/components/Banner/getProps.js
@@ -1,8 +1,8 @@
 // import separateLinkProps from '../separateLinkProps';
 
 export default function getBannarProps({ _lismClass = [], variant, ...props }) {
-	_lismClass.push('c--banner');
-	if (variant) _lismClass.push(` c--banner--${variant}`);
+	_lismClass.push('b--banner');
+	if (variant) _lismClass.push(` b--banner--${variant}`);
 
 	return { _lismClass, ...props };
 }
--- packages/lism-core/src/components/__Banner/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Banner } from './Banner';
-// export { default as BannerContent } from './BannerContent';
