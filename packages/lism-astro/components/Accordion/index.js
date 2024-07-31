83350e06b34534eaad0008e086f5d0ebd728ac80
ddryo <dd.bocsfcryo@gmail.com>
2024-07-31 17:29:43 +0900

    AccordionLabel,Icon削除

--- packages/lism-astro/components/Accordion/index.js
+++ packages/lism-astro/components/Accordion/index.js
@@ -1,6 +1,4 @@
+export { default as AccordionGroup } from './AccordionGroup.astro';
 export { default as Accordion } from './Accordion.astro';
 export { default as AccordionBody } from './AccordionBody.astro';
 export { default as AccordionHeader } from './AccordionHeader.astro';
-export { default as AccordionLabel } from './AccordionLabel.astro';
-export { default as AccordionIcon } from './AccordionIcon.astro';
-export { default as AccordionGroup } from './AccordionGroup.astro';
