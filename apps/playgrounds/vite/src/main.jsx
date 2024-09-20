6ff299086917dc7b0272d616849cb473e5bae14b
ddryo <dd.bocsfcryo@gmail.com>
2024-09-20 22:42:40 +0900

    App.local戻す

--- apps/playgrounds/vite/src/main.jsx
+++ apps/playgrounds/vite/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.local.jsx';
+import App from './App.jsx';
 // import './index.css'
 import '@lism-ui/core/css/all.css';
 
