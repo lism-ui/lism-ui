8e02e793a37f9623da156e105c5a6ab1dce62df1
ddryo <dd.bocsfcryo@gmail.com>
2025-05-19 02:36:27 +0900

    docs編集

--- apps/docs/src/components/ex/Avatar/index.jsx
+++ apps/docs/src/components/ex/Avatar/index.jsx
@@ -1,8 +1,8 @@
 import { Media, Frame } from '@lism-ui/core';
 
-export default function Avatar({ src = '', alt = '', ...props }) {
+export default function Avatar({ size = '2em', src = '', alt = '', ...props }) {
 	return (
-		<Frame skipState lismClass='b--avatar' ar='1/1' w='2em' h='auto' bdrs='99' {...props}>
+		<Frame lismClass='b--avatar' ar='1/1' w={size} h='auto' bdrs='99' {...props}>
 			<Media src={src} alt={alt} width='100%' height='100%' decoding='async' />
 		</Frame>
 	);
