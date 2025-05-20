027596a0667a9cbb3670de09822ac8f40de80a8e
ddryo <dd.bocsfcryo@gmail.com>
2025-05-20 10:30:41 +0900

    Chat調整

--- apps/docs/src/components/ex/Chat/index.jsx
+++ apps/docs/src/components/ex/Chat/index.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Lism, GridItem, Grid, Frame, Media, Decorator } from '@lism-ui/core';
+import './style.scss';
 
-export function Chat({
+export default function Chat({
 	variant = 'speak',
 	direction = 'start',
 	name,
 	avatar,
-	isFlow = true,
-	children,
+	isFlow,
 	keycolor = 'gray',
+	children,
 	...props
 }) {
-	let lismClass = `c--chat`;
-	if (variant) lismClass += ` c--chat--${variant}`;
-	let Layout = Grid;
+	// let lismClass = `c--chat`;
+	// if (variant) lismClass += ` c--chat--${variant}`;
 
 	return (
-		<Layout
-			lismClass={lismClass}
+		<Grid
+			lismClass='c--chat'
+			variant={variant}
 			keycolor={keycolor}
 			bg='none'
 			data-chat-direction={direction}
@@ -25,9 +26,9 @@ export function Chat({
 			{...props}
 		>
 			{avatar && (
-				<Frame
+				<GridItem
+					layout={Frame}
 					lismClass='c--chat__avatar'
-					isSide
 					src={avatar}
 					alt=''
 					bgc='base'
@@ -36,10 +37,10 @@ export function Chat({
 					aria-hidden='true'
 				>
 					<Media src={avatar} alt='' width='60' height='60' decoding='async' />
-				</Frame>
+				</GridItem>
 			)}
 			{name && (
-				<Lism
+				<GridItem
 					lismClass='c--chat__name'
 					c='text-2'
 					fs='i'
@@ -50,20 +51,20 @@ export function Chat({
 					aslf='e'
 				>
 					{name}
-				</Lism>
+				</GridItem>
 			)}
 			<GridItem lismClass='c--chat__body' pos='r'>
-				{variant !== 'box' && (
-					<Decorator
-						lismClass='c--chat__deco u--colored'
-						lismState={['has--mask u--skipFlow']}
-						pos='a'
-						insets={direction === 'start' ? { ie: '100%' } : { is: '100%' }}
-						scale={direction === 'start' ? '' : '-X'}
-					/>
-				)}
+				<Decorator
+					lismClass='c--chat__deco'
+					className='u--colored u--skipFlow'
+					hasMask
+					pos='a'
+					insets={direction === 'start' ? { ie: '100%' } : { is: '100%' }}
+					scale={direction === 'start' ? '' : '-X'}
+				/>
 				<Lism
-					lismClass='c--chat__content u--colored u--trimHL'
+					lismClass='c--chat__content'
+					className='u--colored u--trimHL'
 					bdrs='4'
 					p='30'
 					isFlow={isFlow}
@@ -72,6 +73,6 @@ export function Chat({
 					{children}
 				</Lism>
 			</GridItem>
-		</Layout>
+		</Grid>
 	);
 }
