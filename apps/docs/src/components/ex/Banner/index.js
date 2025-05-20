a98164f5122ed903d034903fb482797b2ceeb13d
ddryo <dd.bocsfcryo@gmail.com>
2025-05-20 10:35:30 +0900

    exDoc調整

--- apps/docs/src/components/ex/Banner/index.js
+++
@@ -1,2 +0,0 @@
-// export { default as Banner } from './index.jsx';
-export { default as Banner } from './index.astro';
--- apps/docs/src/components/ex/Banner/index.jsx
+++
@@ -1,5 +0,0 @@
-import { LinkBox, Frame } from '@lism-ui/core';
-
-export default function Banner(props) {
-	return <LinkBox layout={Frame} lismClass='c--banner' {...props} />;
-}
