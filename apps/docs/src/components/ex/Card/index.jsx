3a32637eca57bd35eaefa24887be808a20ca6de3
ddryo <dd.bocsfcryo@gmail.com>
2025-05-22 12:34:23 +0900

    Card調整

--- apps/docs/src/components/ex/Card/index.jsx
+++ apps/docs/src/components/ex/Card/index.jsx
@@ -1,10 +1,9 @@
-import { Stack, Frame, WithSide } from '@lism-ui/core';
+import { WithSide } from '@lism-ui/core';
 
-export function Card({ layout, ...props }) {
+export default function Card({ layout, ...props }) {
 	const Layout = layout || WithSide;
 
 	const defaultProps = {
-		lismClass: 'c--card',
 		c: 'text',
 		bgc: 'base',
 		bdrs: '3',
@@ -12,22 +11,11 @@ export function Card({ layout, ...props }) {
 		ov: 'h',
 	};
 
+	// hrefが指定されていればlink化
 	if (props.href) {
 		defaultProps.tag = 'a';
 		defaultProps.isLinkBox = true;
 	}
 
-	return <Layout {...defaultProps} {...props} />;
-}
-
-export function CardMedia(props) {
-	return <Frame lismClass='c--card__media' {...props} />;
-}
-
-export function CardBody({ children, ...props }) {
-	return (
-		<Stack lismClass='c--card__body' {...props}>
-			{children}
-		</Stack>
-	);
+	return <Layout lismClass='c--card' {...defaultProps} {...props} />;
 }
