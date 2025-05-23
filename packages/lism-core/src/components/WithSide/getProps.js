874ed1761e65840d9660367a60a7a5fde7a0b392
ddryo <dd.bocsfcryo@gmail.com>
2025-05-23 22:30:39 +0900

    WithSideを修正

--- packages/lism-core/src/components/WithSide/getProps.js
+++ packages/lism-core/src/components/WithSide/getProps.js
@@ -1,35 +1,18 @@
 import atts from '../../lib/helper/atts';
 import { mergeFlexContextProps } from '../Flex/getProps';
 
-export function getWithSideProps({
-	lismClass,
-	style = {},
-	sidePosition,
-	sideW,
-	mainMinW,
-	bp,
-	...props
-}) {
-	let grid = {};
-	if (sidePosition) {
-		props['data-side-position'] = sidePosition;
-		if (bp) {
-			grid = { gd: {} };
-			grid.gd[bp] = 'var(--gd--hz)';
-		}
-	}
+export function getWithSideProps({ lismClass, sideW, mainW, style = {}, ...props }) {
 	if (null != sideW) {
 		style['--side-w'] = sideW;
 	}
-	if (null != mainMinW) {
-		style['--main-minW'] = mainMinW;
+	if (null != mainW) {
+		style['--main-w'] = mainW;
 	}
 
 	return Object.assign(
 		{
 			lismClass: atts(lismClass, `l--withSide`),
 			style,
-			grid,
 		},
 		mergeFlexContextProps(props)
 	);
